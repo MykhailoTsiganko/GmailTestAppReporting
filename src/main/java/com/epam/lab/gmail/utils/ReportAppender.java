7c5240a31b574cc57a61e0bfa3acd0a9d7d720ee
Mykahilo Tsyhanko <mihailo.tsyhanko@gmail.com>
2017-08-05 17:21:18 +0300

    dfsgdfsgdfs

--- src/main/java/com/epam/lab/gmail/utils/ReportAppender.java
+++ src/main/java/com/epam/lab/gmail/utils/ReportAppender.java
@@ -2,7 +2,6 @@ package com.epam.lab.gmail.utils;
 
 import org.apache.log4j.AppenderSkeleton;
 import org.apache.log4j.Layout;
-import org.apache.log4j.Level;
 import org.apache.log4j.spi.LoggingEvent;
 import org.testng.Reporter;
 
@@ -24,8 +23,7 @@ public class ReportAppender extends AppenderSkeleton {
 				}
 			}
 		}
-			
-	
+
 		return buff.toString();
 	}
 
