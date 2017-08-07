a44c166f6a1471e46a1b853b4ed78f7e750114e3
Mykahilo Tsyhanko <mihailo.tsyhanko@gmail.com>
2017-08-07 14:39:06 +0300

    mycomm

--- src/test/java/com/epam/lab/gmail/listeners/LoggerListener.java
+++ src/test/java/com/epam/lab/gmail/listeners/LoggerListener.java
@@ -5,48 +5,47 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
-public class LoggerListener implements ITestListener{
+public class LoggerListener implements ITestListener {
 	public static Logger logger = Logger.getLogger(LoggerListener.class);
 
 	@Override
 	public void onTestStart(ITestResult result) {
-		logger.info(String.format("Method[%-30s], Starts in : %d", result.getMethod().getMethodName()));
+		logger.info(String.format("Test : %s , Method : %s, Started", result.getName(), result.getMethod()));
 
 	}
 
 	@Override
 	public void onTestSuccess(ITestResult result) {
-		logger.info(result.getTestClass().toString().toUpperCase());
-		
+		logger.info(String.format("Test : %s , Method : %s passed successful", result.getName(),
+				result.getMethod()));
 	}
 
 	@Override
 	public void onTestFailure(ITestResult result) {
-		logger.warn(result.getName().toString().toUpperCase() + "FAILURE");
-		logger.warn(result.getThrowable());
-		
+		logger.warn(String.format("Test : %s , Method : %s failed in", result.getName(), result.getMethod()));
 	}
 
 	@Override
 	public void onTestSkipped(ITestResult result) {
-		logger.info(result.toString());
-		
+		logger.info(String.format("Test : %s , Method : %s was skipped ", result.getName(), result.getMethod()));
 	}
 
 	@Override
 	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
-		logger.info(result.toString());
-		
+		logger.warn(String.format("Test : %s , Method : %s failed  within success precentage: %s  ",
+				result.getName(), result.getMethod(),result.getTestContext().getFailedButWithinSuccessPercentageTests().getAllResults().toString()));
 	}
 
 	@Override
 	public void onStart(ITestContext context) {
-		
+		logger.info(String.format("Starting tests : %s, Output directory : %s, Test Suite : %s",
+				context.getName(), context.getOutputDirectory(), context.getSuite()));
 	}
 
 	@Override
 	public void onFinish(ITestContext context) {
-		
+		logger.info(String.format("Finish tests : %s, Output directory : %s, Test Suite : %s",
+				context.getName(), context.getOutputDirectory(), context.getSuite()));
 	}
 
 }
